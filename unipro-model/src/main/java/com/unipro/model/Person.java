95911e9a9dc17c3bb442e47bbd0edc8433d63821
juanfe <juanfezero@gmail.com>
2013-08-05 15:50:36 +0200

    creation on uniproproject organization

---
+++ unipro-model/src/main/java/com/unipro/model/Person.java
@@ -0,0 +1,28 @@
+package com.unipro.model;
+
+public class Person {
+	
+	private String name, password;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
